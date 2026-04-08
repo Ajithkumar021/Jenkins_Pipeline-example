0f0c29d8d434a23b3fde24fb9e197113460c1fc2
Ajithkumar021 <97010854+Ajithkumar021@users.noreply.github.com>
2026-04-08 19:37:07 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+
+//Root route
+app.get('/', (req, res) => {
+    res.send('WORKING CODE');
+});
+
+// Health check route
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
